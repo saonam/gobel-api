331593264cd999db5256e860f1c6c08c10aecf0a
bmf-san <bmf.infomation@gmail.com>
2020-05-17 01:40:04 +0900

    tmp

--- app/infrastructure/logger.go
+++ app/infrastructure/logger.go
@@ -1,62 +1,112 @@
 package infrastructure
 
 import (
-	"io"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/bmf-san/gobel-api/app/usecases"
 	"github.com/pkg/errors"
 )
 
-// errorLogFile is path to error log file.
-const errorLogFile = "./log/error.log"
+// Level is log levels.
+type Level int8
 
-// accessLogFile is path to access log file.
-const accessLogFile = "./log/access.log"
+const (
+	// FatalLevel is an abend error.
+	FatalLevel Level = iota
+	// ErrorLevel is unexpected runtime error.
+	ErrorLevel
+	// WarnLevel is warning.
+	WarnLevel
+	// InfoLevel is something notable infomation.
+	InfoLevel
+)
+
+// String converts constant to string.
+func (l Level) String() string {
+	switch l {
+	case FatalLevel:
+		return "trace"
+	case ErrorLevel:
+		return "debug"
+	case WarnLevel:
+		return "info"
+	case InfoLevel:
+		return "warn"
+	}
+	return ""
+}
 
 // A Logger represents a logger.
-type Logger struct {
-	errorLogFile  string
-	accessLogFile string
-}
+type Logger struct{}
 
 // NewLogger creates a logger.
 func NewLogger() usecases.Logger {
-	return &Logger{
-		errorLogFile:  errorLogFile,
-		accessLogFile: accessLogFile,
-	}
+	// TODO: set option?
+	return &Logger{}
 }
 
 // LogError writes a log for an error log.
 func (l *Logger) LogError(e error) {
-	file, err := os.OpenFile(l.errorLogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Printf("%+v\n", errors.WithStack(err))
-	}
-	defer file.Close()
-
-	// TODO: Optimize log format
-	log.SetOutput(io.MultiWriter(file, os.Stdout))
-	log.SetFlags(log.Ldate | log.Ltime)
-
-	log.Printf("%+v\n", errors.WithStack(e))
+	set(os.Stderr)
+	write()
 }
 
 // LogAccess writes a log for an access log.
 func (l *Logger) LogAccess(r *http.Request) {
-	// TODO: To implement log lotation.
-	file, err := os.OpenFile(l.accessLogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Printf("%+v\n", errors.WithStack(err))
+	set(os.Stdout)
+
+	startTime := time.Now()
+	writer := &LoggingWriter{
+		ResponseWriter: rw,
+		logRecord: LogRecord{
+			Time:          startTime.UTC(),
+			Ip:            ip,
+			Method:        r.Method,
+			Uri:           r.RequestURI,
+			Username:      username,
+			Protocol:      r.Proto,
+			Host:          r.Host,
+			Status:        0,
+			Size:          0,
+			ElapsedTime:   time.Duration(0),
+			RequestHeader: r.Header,
+		},
 	}
-	defer file.Close()
 
-	// TODO: Optimize log format
-	log.SetOutput(io.MultiWriter(file, os.Stdout))
-	log.SetFlags(log.Ldate | log.Ltime)
+	if h.logBefore {
+		writer.SetCustomLogRecord("at", "before")
+		h.logger.LogContext(r.Context(), writer.logRecord)
+	}
 
+	ctx := context.WithValue(r.Context(), ctxLoggerKey, writer)
+	r = r.WithContext(ctx)
+	h.handler.ServeHTTP(writer, r)
+	finishTime := time.Now()
+
+	writer.logRecord.Time = finishTime.UTC()
+	writer.logRecord.ElapsedTime = finishTime.Sub(startTime)
+
+	write()
+}
+
+// Set sets logger options.
+func set(writer *os.File) {
+	log.SetOutput(writer)
+	// TODO: 後で不要なやつ削る
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Llongfile | log.Lshortfile | log.LUTC)
+}
+
+// Write writes logs.
+func write() {
+	// err
+	log.Printf("%+v\n", errors.WithStack(e))
+
+	// access
 	log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+
+	log.Printf()
 }
